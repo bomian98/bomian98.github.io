3415ebcbc9edca59f4bc3452609a8c8a79b6365e
王世超 <wangshichao@4paradigm.com>
2024-02-20 11:17:39 +0800

    import import { getDirname, path } from "vuepress/utils" error

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,5 +1,8 @@
 import { defineUserConfig } from "vuepress";
+import { getDirname, path } from "vuepress/utils";
 import theme from "./theme.js";
+
+const __dirname = getDirname(import.meta.url);
 
 export default defineUserConfig({
   base: "/",
@@ -10,6 +13,14 @@ export default defineUserConfig({
 
   theme,
 
+  alias: {
+    // 你可以在这里将别名定向到自己的组件
+    // 比如这里我们将主题的主页组件改为用户 .vuepress/components 下的 HomePage.vue
+    "@theme-hope/components/HomePage": path.resolve(
+      __dirname,
+      "./components/HomePage.vue",
+    ),
+  },
   // 和 PWA 一起启用
   // shouldPrefetch: false,
 });
