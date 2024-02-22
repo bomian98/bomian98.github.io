a42b8988cf8ddfeed138d0f30e7cb641d65c90e3
wsczh <1998wsc@gmail.com>
2024-02-22 21:52:34 +0800

    change font

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -8,4 +8,18 @@ export default defineUserConfig({
   title: "博客演示",
   description: "vuepress-theme-hope 的博客演示",
   theme,
+  head: [
+    ["link", { rel: "preconnect", href: "https://fonts.googleapis.com" }],
+    [
+      "link",
+      { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" },
+    ],
+    [
+      "link",
+      {
+        href: "https://fonts.googleapis.com/css2?family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap",
+        rel: "stylesheet",
+      },
+    ],
+  ],
 });
